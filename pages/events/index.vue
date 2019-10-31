a385d12ea2ccd7bb8ec579ae43c09deda455f8ef
cmblue <german.sayago@gmail.com>
2019-10-31 10:44:43 -0300

    Component loading added

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
+    <loading v-if="loading" />
     <div v-for="event in events" :key="event.id" class="event">
-      {{ event.title }}
+      <div>titulo <b>{{ event.id }}</b></div>
+      <div>titulo <b>{{ event.title }}</b></div>
+      <div>group_id <b>{{ event.group_id }}</b></div>
+      <div>state <b>{{ event.state }}</b></div>
       <nuxt-link :to="`/events/${event.id}`">
         ver
       </nuxt-link>
@@ -11,18 +15,26 @@
 
 <script>
 import axios from 'axios'
+import Loading from '~/components/Loading.vue'
 
 export default {
+  components: {
+    Loading
+  },
+
   data () {
     return {
-      events: []
+      events: [],
+      loading: true
     }
   },
+
   asyncData ({ params, error }) {
     return axios.get(`http://admin.visitriocuarto.com/api/events`)
       .then((res) => {
         return {
-          events: res.data.result
+          events: res.data.result,
+          loading: false
         }
       })
   }
