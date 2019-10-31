dc0cedbe6d0341e2b120f960622bcb400ccbc611
cmblue <german.sayago@gmail.com>
2019-10-31 15:03:20 -0300

    was replaced the folder events for eventos

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <div class="container">
       <a class="navbar-brand" href="/">
         <img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
@@ -27,7 +27,7 @@
             <a class="nav-link" href="/about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/events">Events</a>
+            <a class="nav-link" href="/eventos">Eventos</a>
           </li>
         </ul>
       </div>
