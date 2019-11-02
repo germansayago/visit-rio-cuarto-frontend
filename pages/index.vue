fb99bf05068f1fefeae7229e7574e77f25e29bb6
German Sayago <german.sayago@gmail.com>
2019-11-02 10:52:50 -0300

    Search was added in homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,95 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        home
-      </h1>
-      <h2 class="subtitle">
-        Visit Rio Cuarto
-      </h2>
-      <div>
-        <i class="fas fa-coffee fa-xs"></i>
+    <section class="section">
+      <div class="jumbotron">
+        <h1>{{ titlePage.toUpperCase() }}</h1>
+        <div class="form-group">
+          <input
+            v-model="searchQuery"
+            type="text"
+            class="form-control form-control-lg"
+            placeholder="Que querés hacer?"
+          >
+        </div>
+        <button @click="search" class="btn btn-success btn-lg">buscar</button>
+        <button @click="clean" class="btn btn-info btn-lg">reset</button>
       </div>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+
+      <div v-if="searchMessage > 0" class="row">
+        <div class="col">
+          <div class="alert alert-success">
+            Se han encontrado {{ searchMessage }} resultados
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div class="row">
+        <div class="col">
+          <div v-for="(e, index) in events" :key="index" class="card mb-3">
+            <div class="card-body">
+              <h3 class="card-title">{{ e.title }}</h3>
+              <p>{{ e.place }}</p>
+            </div>
+            <div class="card-footer">
+              <p>{{ e.category }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+const events = [
+  {
+    title: 'La noche de los museos',
+    place: 'Museo de Bellas Artes',
+    category: 'Cultura y Espectáculos'
+  },
+  {
+    title: 'Expo turismo',
+    place: 'Sociedad Rural de Río Cuarto',
+    category: 'Turismo'
+  },
+  {
+    title: 'Estudiantes vs Belgrano',
+    place: 'Asociación Atlética Estudiantes',
+    category: 'Deportes'
+  }
+]
 
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      titlePage: 'Viví la experiencia!',
+
+      searchQuery: '',
+
+      events: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `${this.events.length}`
+    }
+  },
+
+  methods: {
+    search () {
+      this.events = events
+    },
+
+    clean () {
+      this.events = []
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  body{
+    background-color: #fff;
+  }
+</style>
